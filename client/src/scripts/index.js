38bbc0c5d9e628cc9cb9fc6c0c18b44d8b56e3fc
Robby <ta7382@gmail.com>
2018-03-12 23:36:52 +0800

    Update

--- client/src/scripts/index.js
+++ client/src/scripts/index.js
@@ -4,5 +4,9 @@ if (module.hot) {
   module.hot.accept();
 }
 
+import Header from '../components/header/Header';
+
+const a = new Header();
+
 import 'babel-polyfill';
 import '../styles/index.scss';
