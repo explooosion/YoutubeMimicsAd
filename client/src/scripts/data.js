d2f92f9e13f4b8529bf8ebcea85d273d10123336
Robby <ta7382@gmail.com>
2018-03-13 23:42:20 +0800

    Update selector

---
+++ client/src/scripts/data.js
@@ -0,0 +1,45 @@
+const data = [{
+    type: 'sport',
+    title: 'Jeremy Lin 7 Breakthrough Games Highlights *林書豪7場全紀錄 HD *New York Knicks win 7 games',
+    url: 'https://www.youtube.com/watch?v=reOuvzhtuxs',
+    src: '林書豪7場全紀錄 HD New York Knicks win 7 games.mp4',
+    avatar: 'sport.jpg',
+    poster: 'sport.jpg',
+}, {
+    type: 'travel',
+    title: '《微VLOG》金字塔與象形文字的探險！埃及十天超精華微VLOG！【聖結石Saint】',
+    url: 'https://www.youtube.com/watch?v=S57TVyJqNZ0',
+    src: '《微VLOG》金字塔與象形文字的探險！埃及十天超精華微VLOG！【聖結石Saint】.mp4',
+    avatar: 'travel.jpg',
+    poster: 'travel.jpg',
+}, {
+    type: 'game',
+    title: '【DE JuN】絕地求生刺客流!!※平底鍋殺手 #1',
+    url: 'https://www.youtube.com/watch?v=-a5jk3W3D2c',
+    src: 'DEJuN絕地求生刺客流-平底鍋殺手.mp4',
+    avatar: 'game.jpg',
+    poster: 'game.jpg',
+}, {
+    type: 'makeups',
+    title: '《新手必看》保溼全攻略。博士教你挑產品。【Dr. Ivan6】How to choose the Moisturizing product ?',
+    url: 'https://www.youtube.com/watch?v=hfi-dmZ_Jsk',
+    src: '《新手必看》保溼全攻略。博士教你挑產品。【Dr. Ivan6】How to choose the Moisturizing product _.mp4',
+    avatar: 'makeups.jpg',
+    poster: 'makeups.jpg',
+}, {
+    type: 'food',
+    title: '不韓而栗 韓國明洞美食餐廳超級詳細路線圖~!!!!!!',
+    url: 'https://www.youtube.com/watch?v=9J9zZNJ1lIw',
+    src: '不韓而栗 韓國明洞美食餐廳超級詳細路線圖_!!!!!!.mp4',
+    avatar: 'food.jpg',
+    poster: 'food.jpg',
+}, {
+    type: 'movie',
+    title: '【谷阿莫】9分鐘看完900分鐘網路劇《貼身校花》',
+    url: 'https://www.youtube.com/watch?v=VRoW6SESnl0',
+    src: '【谷阿莫】9分鐘看完900分鐘網路劇《貼身校花》.mp4',
+    avatar: 'movie.jpg',
+    poster: 'movie.jpg',
+}]
+
+export default data
